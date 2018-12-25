eaccae9bea2de2169d54aff68b5598f61f2b0ba9
jacovis <jaco.vis@concepts.nl>
2018-12-25 02:01:33 +0100

    Initial commit after extracting this code from solution Javi.

--- release-nuget-package.cmd
+++ release-nuget-package.cmd
@@ -1,5 +1,5 @@
 rem https://docs.microsoft.com/en-us/nuget/quickstart/create-and-publish-a-package-using-visual-studio
 
-rd /s /q .\Javi.FFmpeg\bin\Release
-dotnet build -c Release .\Javi.FFmpeg\Javi.FFmpeg.csproj
-nuget push .\Javi.FFmpeg\bin\Release\*.nupkg -Source https://api.nuget.org/v3/index.json
+rd /s /q .\Javi.MediaInfo\bin\Release
+dotnet build -c Release .\Javi.FFmpeg\Javi.MediaInfo.csproj
+nuget push .\Javi.MediaInfo\bin\Release\*.nupkg -Source https://api.nuget.org/v3/index.json
